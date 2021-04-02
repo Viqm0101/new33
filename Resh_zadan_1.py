60bf059a35ead52b2c60415f0a31825fc4147bb0
Viqm <viqm@mail.ru>
2021-04-02 19:30:10 +0300

    0444

---
+++ Resh_zadan_1.py
@@ -0,0 +1,2 @@
+from zadan_1 import calc_rab
+print(calc_rab())
---
+++ zadan_1.py
@@ -0,0 +1,12 @@
+def calc_rab():
+    x = 250
+    y = 200
+    b = x * y + 15000
+    return b
+
+print(calc_rab())
+
+pol = int(input('едите число!!: '))
+bet = int(input('введите ставку: '))
+
+print(pol + bet)
